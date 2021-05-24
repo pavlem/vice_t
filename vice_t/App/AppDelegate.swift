b15a0bda1edbce3be38fd907a2ec120147fe4f08
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-05-24 10:41:17 +0200

    Initial setup

---
+++ vice_t/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  vice_t
+//
+//  Created by Pavle Mijatovic on 24.5.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ vice_tTests/vice_tTests.swift
@@ -0,0 +1,33 @@
+//
+//  vice_tTests.swift
+//  vice_tTests
+//
+//  Created by Pavle Mijatovic on 24.5.21..
+//
+
+import XCTest
+@testable import vice_t
+
+class vice_tTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
